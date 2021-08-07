92519f6dee2f2017c566b59a56769ad26b72c995
Aliya Merali <merali4@gmail.com>
2021-08-07 17:32:24 -0600

    Refactor controller into image_facade

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,26 +1,6 @@
 class Api::V1::BackgroundController < ApplicationController
   def show
-    location = params[:location]
-    weather_condition = ForecastFacade.get_forecast(location).current_weather[:conditions]
-    time_of_day = time_of_day(Time.now.hour)
-    images = ImageService.search(location+' '+weather_condition+' '+time_of_day)
-    image = images[:results].sample
-    image_object = Image.new(image, location)
-    render json: ImageSerializer.new(image_object)
-  end
-
-  private
-  def time_of_day(hour)
-    if hour <= 5
-      'night'
-    elsif hour <= 12
-      'morning'
-    elsif hour <= 17
-      'afternoon'
-    elsif hour <= 22
-      'evening'
-    elsif hour <= 24
-      'night'
-    end
+    image = ImageFacade.get_image(params[:location])
+    render json: ImageSerializer.new(image)
   end
 end
---
+++ app/facades/image_facade.rb
@@ -0,0 +1,24 @@
+class ImageFacade
+  def self.get_image(location)
+    weather_conditions = ForecastFacade.get_forecast(location).current_weather[:conditions]
+    time_of_day = time_of_day(Time.now.hour)
+    images = ImageService.search(location+' '+weather_conditions+' '+time_of_day)
+    image_data = images[:results].sample
+    Image.new(image_data, location)
+  end
+
+  private
+  def self.time_of_day(hour)
+    if hour <= 5
+      'night'
+    elsif hour <= 12
+      'morning'
+    elsif hour <= 17
+      'afternoon'
+    elsif hour <= 22
+      'evening'
+    elsif hour <= 24
+      'night'
+    end
+  end
+end

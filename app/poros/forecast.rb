0159ec3484f10703b1587b538e3ed590f06ae669
Aliya Merali <merali4@gmail.com>
2021-08-07 15:47:05 -0600

    Remove local time from time converstion to eliminate error

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -8,9 +8,9 @@ class Forecast
   end
 
   def get_current_weather(data)
-    { datetime: Time.at.getlocal(data[:dt]),
-      sunrise: Time.at.getlocal(data[:sunrise]),
-      sunset: Time.at.getlocal(data[:sunset]),
+    { datetime: Time.at(data[:dt]),
+      sunrise: Time.at(data[:sunrise]),
+      sunset: Time.at(data[:sunset]),
       temperature: data[:temp],
       feels_like: data[:feels_like],
       humidity: data[:humidity],
@@ -23,7 +23,7 @@ class Forecast
   def get_hourly_weather(hourly_data)
     hourly_data.drop(1).take(8).map do |hour_data|
       {
-        time: Time.at.getlocal(hour_data[:dt]).strftime('%I:%M:%S'),
+        time: Time.at(hour_data[:dt]).strftime('%I:%M:%S'),
         temperature: hour_data[:temp],
         conditions: hour_data[:weather].first[:description],
         icon: hour_data[:weather].first[:icon]
@@ -33,9 +33,9 @@ class Forecast
 
   def get_daily_weather(daily_data)
     daily_data.drop(1).take(5).map do |day_data|
-      { date: Time.at.getlocal(day_data[:dt]).strftime('%Y-%m-%d'),
-        sunrise: Time.at.getlocal(day_data[:sunrise]),
-        sunset: Time.at.getlocal(day_data[:sunset]),
+      { date: Time.at(day_data[:dt]).strftime('%Y-%m-%d'),
+        sunrise: Time.at(day_data[:sunrise]),
+        sunset: Time.at(day_data[:sunset]),
         max_temp: day_data[:temp][:max],
         min_temp: day_data[:temp][:min],
         conditions: day_data[:weather].first[:description],

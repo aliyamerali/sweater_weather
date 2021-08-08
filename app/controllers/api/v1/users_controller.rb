244a5b1479bc8b60ee085c04815b59e4bc0c80ce
Aliya Merali <merali4@gmail.com>
2021-08-08 07:53:03 -0600

    Set successful response code to created, resolve rubocop offenses

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,4 @@
 require 'securerandom'
-
 class Api::V1::UsersController < ApplicationController
   def create
     user = user_params
@@ -7,13 +6,14 @@ class Api::V1::UsersController < ApplicationController
     user[:api_key] = SecureRandom.urlsafe_base64(27)
     new_user = User.create(user)
     if new_user.save
-      render json: UserSerializer.new(new_user)
+      render json: UserSerializer.new(new_user), status: :created
     else
       render json: { response: new_user.errors }, status: :bad_request
     end
   end
 
   private
+
   def user_params
     params.permit(:email, :password, :password_confirmation)
   end

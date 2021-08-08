13e82f636ef635e0c02a81b11206c84631d3c8dc
Aliya Merali <merali4@gmail.com>
2021-08-07 18:10:43 -0600

    Resolve Rubocop complaints

--- app/facades/image_facade.rb
+++ app/facades/image_facade.rb
@@ -1,14 +1,14 @@
 class ImageFacade
   def self.get_image(location)
     weather_conditions = ForecastFacade.get_forecast(location).current_weather[:conditions]
-    time_of_day = time_of_day(Time.now.hour)
+    time_of_day = time_of_day(Time.current.hour)
     images = ImageService.search("#{location} #{weather_conditions} #{time_of_day}")
     image_data = images[:results].sample
     Image.new(image_data, location)
   end
 
   def self.time_of_day(hour)
-    if hour <= 5
+    if hour <= 5 || (hour <= 24 && hour > 22)
       'night'
     elsif hour <= 12
       'morning'
@@ -16,8 +16,6 @@ class ImageFacade
       'afternoon'
     elsif hour <= 22
       'evening'
-    elsif hour <= 24
-      'night'
     end
   end
 end

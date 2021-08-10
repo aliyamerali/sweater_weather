af6f3a951b59d40684bcd1b124d93c2df452ca9e
Aliya Merali <merali4@gmail.com>
2021-08-10 14:15:54 -0600

    Add test to cover all geocode service conditions

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'Geocode location service' do
         expect(response[:results].first[:locations].first[:latLng]).to eq(expected)
       end
 
-      it 'returns an error if not enough or invalid data given' do
+      it 'returns an error if not enough data given' do
         location = ""
         lat_long_response = File.read('spec/fixtures/lat_long_failure.json')
         stub_request(:get,"http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}").
@@ -35,6 +35,19 @@ RSpec.describe 'Geocode location service' do
         expect(response.error_message).to eq('Illegal argument from request: Insufficient info for location')
       end
 
+      it 'returns an error if invalid data given' do
+        location = "~"
+        lat_long_response = File.read('spec/fixtures/lat_long_failure_symbol.json')
+        stub_request(:get,"http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}").
+            to_return(status: 200, body: lat_long_response, headers: {})
+
+        response = GeocodeService.lat_long(location)
+
+        expect(response).to be_a(Error)
+        expect(response.code).to eq(400)
+        expect(response.status).to eq('Bad Request')
+        expect(response.error_message).to eq('Illegal argument from request: Insufficient info for location')
+      end
     end
   end
 end

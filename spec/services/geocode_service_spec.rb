fcbdd3203a28ffd09d17d0bb51a91129a23c815a
Aliya Merali <merali4@gmail.com>
2021-08-10 13:58:54 -0600

    Add error handling and response to forecast functionality, including specs for services and feature

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'Geocode location service' do
         expect(response[:results].first[:locations].first[:latLng]).to eq(expected)
       end
 
-      it 'returns a 400 if not enough data given' do
+      it 'returns an error if not enough or invalid data given' do
         location = ""
         lat_long_response = File.read('spec/fixtures/lat_long_failure.json')
         stub_request(:get,"http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}").
@@ -29,8 +29,10 @@ RSpec.describe 'Geocode location service' do
 
         response = GeocodeService.lat_long(location)
 
-        expect(response[:info][:statuscode]).to eq(400)
-        expect(response[:results].first[:locations]).to eq([])
+        expect(response).to be_a(Error)
+        expect(response.code).to eq(400)
+        expect(response.status).to eq('Bad Request')
+        expect(response.error_message).to eq('Illegal argument from request: Insufficient info for location')
       end
 
     end
